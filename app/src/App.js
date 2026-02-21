a50a0532f7e23d71b8425bb7133ab144b9db5586
kachariyakeyur <kachariyakeyur33@gmail.com>
2026-02-21 12:44:17 +0530

    modes add

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,28 @@
-
+import "./Components/Style.css"
+import Navbar from "./Components/Navbar"
+import Textform from "./Components/Textform";
+import About from "./Components/About.js";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-  <>
-  Hello !
-  </>
+  <Router>
+    <Navbar/>
+   
+    <Routes>
+     
+        <Route exact path="/" element={<Textform />} />
+     
+    
+        <Route exact path="/about" element={<About />} />
+     
+    </Routes>
+
+  </Router>
   );
 }
 
